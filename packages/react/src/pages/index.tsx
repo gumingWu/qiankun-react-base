2978f7abd903ebc9d4168d647cb40678135219e9
gumingWu <15626185054@163.com>
2022-12-28 14:44:07 +0800

    三个应用准备

---
+++ packages/react/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import yayJpg from '../assets/yay.jpg';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h2>Yay! Welcome to umi!</h2>
+      <p>
+        <img src={yayJpg} width="388" />
+      </p>
+      <p>
+        im react child
+      </p>
+    </div>
+  );
+}
